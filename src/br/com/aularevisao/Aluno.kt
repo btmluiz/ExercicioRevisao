96874f2ba3b08f83571d74cc40428aff9a4188fc
mi-sinibaldi <mia_araujo92@hotmail.com>
2020-09-14 21:25:21 -0300

    implementacao das classes Curso e adicao dos metodos na classe Aluno

--- src/br/com/aularevisao/Aluno.kt
+++ src/br/com/aularevisao/Aluno.kt
@@ -1,4 +1,13 @@
 package br.com.aularevisao
 
 class Aluno (var ra:Int,var nome:String,var sobrenome:String){
+
+    fun assistiAula(aula: Aula){
+        println("$nome assistindo a aula ${aula.nomeMateria}")
+    }
+
+    fun fazerLicao(aula: Aula){
+        println("$nome esta fazendo a lição da materia ${aula.nomeMateria}")
+    }
 }
+
---
+++ src/br/com/aularevisao/Curso.kt
@@ -0,0 +1,5 @@
+package br.com.aularevisao
+
+class Curso(var nome: String, var aulaLista: List<Aula>, var listAlunos: List<Aluno>, var professo: Professor) {
+
+}
